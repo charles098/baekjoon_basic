2715fc9856baf86815ab1da3ad7a870065b9ca66
charles098 <55475550+charles098@users.noreply.github.com>
2020-01-27 19:30:48 +0900

    Add files via upload

---
+++ B2193.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int d[91];
+
+int main() {
+	int n;
+	cin >> n;
+
+	d[1] = 1; d[2] = 1;
+
+	for (int i = 3; i <= n; i++)
+		d[i] = d[i - 1] + d[i - 2];
+	
+	cout << d[n];
+
+	return 0;
+}
