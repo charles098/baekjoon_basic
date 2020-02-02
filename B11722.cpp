0ce9da821182f21d671d946805eafe3f8e6752ca
charles098 <55475550+charles098@users.noreply.github.com>
2020-02-02 17:55:01 +0900

    Add files via upload

---
+++ B11722.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int dp[1001];
+	int a[1001];
+	int n;
+	int value;
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+		dp[i] = 1;
+	}
+
+	for (int i = 1; i <= n; i++) 
+		for (int j = 1; j < i; j++) 
+			if (a[i] < a[j] && dp[j] + 1 > dp[i])
+				dp[i] = dp[j] + 1;
+		
+	int max = 0;
+	for (int i = 1; i <= n; i++)
+		max = max > dp[i] ? max : dp[i];
+
+	cout << max;
+
+	return 0;
+}
