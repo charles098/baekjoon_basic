4bb93d265d5ce41fca4720789f96182239fff5ab
charles098 <55475550+charles098@users.noreply.github.com>
2020-02-06 01:55:29 +0900

    Add files via upload

---
+++ B1912.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int dp[100001];
+	int a[100001];
+	int temp;
+	int n;
+
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+		dp[i] = a[i];
+	}
+
+	for (int i = 2; i <= n; i++) {
+		temp = a[i];
+		for (int j = i; j > 1; j--) {
+			temp += a[j - 1];
+			if (dp[i] < temp)
+				dp[i] = temp;
+		}
+	}
+	
+	int m = dp[1];
+	for (int i = 1; i < n; i++)
+		m = max(m, dp[i + 1]);
+
+	cout << m;
+	return 0;
+}
