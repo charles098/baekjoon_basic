51eb26dcce972f768d7694c848bfe8af352506fc
charles098 <55475550+charles098@users.noreply.github.com>
2020-02-07 01:53:23 +0900

    Add files via upload

---
+++ B2133.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	int dp[31] = {1, 0, 3};
+	cin >> n;
+
+	for (int i = 4; i <= n; i++) {
+		if (i % 2 == 0) {
+			dp[i] = 3 * dp[i - 2];
+			for (int j = 2; j <= i - 2; j += 2) {
+				dp[i] += 2 * dp[i - 2 - j];
+			}
+		}
+	}
+
+	cout << dp[n];
+
+	return 0;
+}
