4ae132365da0b1bc17625346190e4dca6354401a
charles098 <55475550+charles098@users.noreply.github.com>
2020-02-18 03:47:36 +0900

    Add files via upload

---
+++ B2875.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n, m, k;
+	cin >> n >> m >> k;
+	
+	while (n != 2 * m) {
+		if (k == 0) break;
+
+		if (n > 2 * m) {
+			k--;
+			n--;
+		}
+		else if(n < 2 * m){
+			k--;
+			m--;
+		}
+	}
+
+	if (k == 0) {
+		if (n >= 2 * m) cout << m;
+		else cout << n / 2;
+	}
+	else {
+		if (k % 3 == 0)
+			cout << m - (k / 3);
+		else
+			cout << m - (k / 3) - 1;
+	}
+	return 0;
+}
