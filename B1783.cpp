7ec0a3ef72f02b480daa2026d80cbf7631131638
charles098 <55475550+charles098@users.noreply.github.com>
2020-02-19 02:26:53 +0900

    Add files via upload

---
+++ B1783.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+	int N, M;
+	cin >> N >> M;
+
+	if (N == 1) cout << 1;
+	else if (N == 2) cout << min(4, (M + 1) / 2);
+	else if (N >= 3) {
+		if (M <= 6) cout << min(4, M);
+		else cout << M - 2;
+	}
+
+	return 0;
+}
