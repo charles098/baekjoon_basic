cb6e8cd55551523e4550d27306262111ab4b27b9
charles098 <55475550+charles098@users.noreply.github.com>
2020-02-29 00:07:03 +0900

    Update and rename B1476.cpp to B1476-날짜 계산.cpp

--- B1476-날짜 계산.cpp
+++ B1476-날짜 계산.cpp
@@ -1,3 +1,4 @@
+// 백준 1476번 - 날짜 계산
 #include <iostream>
 using namespace std;
 
@@ -10,7 +11,7 @@ int main() {
 
 	while (!(e == E && s == S && m == M)) {
 		e++; s++; m++; count++;
-		if (e == 16) e = 1;
+		if (e == 16) e = 1; // 각각 범위 넘어가면 1로 초기화
 		if (s == 29) s = 1;
 		if (m == 20) m = 1;
 	}
