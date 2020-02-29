b032b7fa1941f49826ea72f064c930cfa1494353
charles098 <55475550+charles098@users.noreply.github.com>
2020-02-29 23:02:41 +0900

    Update and rename B9095.cpp to B9095-1,2,3더하기.cpp

--- B9095-1,2,3더하기.cpp
+++ B9095-1,2,3더하기.cpp
@@ -1,3 +1,6 @@
+// 백준 9095번 - 1, 2, 3 더하기
+// 입력 숫자가 10 이하라서 그냥 손으로 계산 후 벡터에 넣어버렸다
+// 사실 d[n] = d[i - 1] + d[i - 2] + d[i - 3] 이런 규칙이 있기는 하다
 #include <iostream>
 #include <vector>
 using namespace std;
