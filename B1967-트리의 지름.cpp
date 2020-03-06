a7bc2af5585ea63fc31f46b1cd91b2ab047b9b49
charles098 <55475550+charles098@users.noreply.github.com>
2020-03-06 23:23:57 +0900

    Update and rename B1107.cpp to B1967-트리의 지름.cpp

--- B1967-트리의 지름.cpp
+++ B1967-트리의 지름.cpp
@@ -1,3 +1,6 @@
+// 백준 1967번 - 트리의 지름
+// 완전 동일한 문제가 동일한 제목으로 있다
+// 다른 '트리의 지름'문제를 복사해왔다. 다른점은 tree에서 from->to, to->from을 다 연결해줘야 한다는 것. 그 외에는 코드도 똑같다.
 #include <iostream>
 #include <vector>
 using namespace std;
