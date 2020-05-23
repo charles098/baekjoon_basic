ac5ec6ba6d782a1da044f2d4e88b20ec584470a1
charles098 <55475550+charles098@users.noreply.github.com>
2020-05-24 00:40:13 +0900

    Update and rename practice.cpp to B6416-트리인가?.cpp

--- B6416-트리인가?.cpp
+++ B6416-트리인가?.cpp
@@ -1,4 +1,8 @@
-﻿#include <iostream>
+// 백준 6416번 - 트리인가? 
+// 노드의 개수 = 간선의 개수 + 1, 아무 것도 없을 때도 트리임을 이용
+// set을 사용하면 쉽게 풀 수 있다
+// 한 쌍이 입력될때마다 간선은 하나씩 증가, 부모 노드는 중복될 수 있고 자식 노드는 unique하다. 이는 입력 과정에서 set이 알아서 처리해줌.
+#include <iostream>
 #include <set>
 using namespace std;
 
