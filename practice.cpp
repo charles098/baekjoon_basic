98ba449e18593af5b3d2f4bf0ce80460855d8bc0
charles098 <55475550+charles098@users.noreply.github.com>
2020-05-24 00:36:06 +0900

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+	
+	int count = 0;
+	int parent, child, branch;
+	bool IsContinue = true;
+	set<int> tree;
+
+	while (IsContinue) {
+		tree.clear();
+		branch = 0;
+		while (1) {
+			cin >> parent >> child;
+			if (parent == 0 && child == 0) {
+				count++;
+				if(tree.size() == 0 || branch+1 == tree.size())
+					cout << "Case " << count << " is a tree." << endl;
+				else
+					cout << "Case " << count << " is not a tree." << endl;
+				break;
+			}
+			if (parent < 0 && child < 0) {
+				IsContinue = false;
+				break;
+			}
+			tree.insert(parent), tree.insert(child);
+			branch++;
+		}
+	}
+
+	return 0;
+}
