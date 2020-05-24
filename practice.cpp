cadaccc0c80a0e31ed0832448507b69a4972fd27
charles098 <55475550+charles098@users.noreply.github.com>
2020-05-24 16:34:57 +0900

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <set>
+using namespace std;
+
+
+
+int main() {
+	set<string, greater<string>> s;
+	set<string>::iterator iter;
+	int count;
+	string name, log;
+	cin >> count;
+	for (int i = 0; i < count; i++) {
+		cin >> name >> log;
+		iter = s.find(name);
+		if (iter != s.end()) // 존재하는 경우
+			s.erase(iter);
+		else // 처음인 경우
+			s.insert(name);
+	}
+	for (iter = s.begin(); iter != s.end(); iter++)
+		cout << *iter << endl;
+
+	return 0;
+}
